1b80d73018a6e71d0360f731f71bad275339b56a
Kate McCarney <s5432310@student.ad.griffith.edu.au>
2026-05-22 12:51:43 +1000

    Updated UI

---
+++ Source/InterProg_A2/Public/MapManager/MapManagerSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MapManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERPROG_A2_API UMapManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
