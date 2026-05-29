0ce261c7b62213683bdd28ccdc792cb008ec6a0c
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2026-05-29 10:04:41 +1000

    Level transitions

--- Source/InterProg_A2/InterProg_A2.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "InterProg_A2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InterProg_A2, "InterProg_A2" );
